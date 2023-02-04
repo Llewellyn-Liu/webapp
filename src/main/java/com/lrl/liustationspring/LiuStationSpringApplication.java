cfbf77ba3a03bb9083ee9aec9b89a65d8942d775
Llewellyn-Liu <liurunlin0213@live.com>
2023-02-03 20:18:33 -0800

    add css for login.html

---
+++ src/main/java/com/lrl/liustationspring/LiuStationSpringApplication.java
@@ -0,0 +1,15 @@
+package com.lrl.liustationspring;
+
+import org.apache.logging.log4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuStationSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuStationSpringApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lrl/liustationspring/dao/pojo/userREST/AccountCreatedREST.java
@@ -0,0 +1,60 @@
+package com.lrl.liustationspring.dao.pojo.userREST;
+
+import java.sql.Timestamp;
+
+public class AccountCreatedREST extends FieldREST{
+    private static String type = "int";
+
+    private Timestamp value;
+
+    private boolean readOnly;
+
+    private boolean writeOnly;
+
+    public AccountCreatedREST(Timestamp value, boolean readOnly, boolean writeOnly) {
+        this.value = value;
+        this.readOnly = readOnly;
+        this.writeOnly = writeOnly;
+    }
+
+    public static String getType() {
+        return type;
+    }
+
+    public static void setType(String type) {
+        AccountCreatedREST.type = type;
+    }
+
+    public Timestamp getValue() {
+        return value;
+    }
+
+    public void setValue(Timestamp value) {
+        this.value = value;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
+    }
+
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+    }
+
+    public boolean isWriteOnly() {
+        return writeOnly;
+    }
+
+    public void setWriteOnly(boolean writeOnly) {
+        this.writeOnly = writeOnly;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountCreatedREST{" +
+                "value=" + value +
+                ", readOnly=" + readOnly +
+                ", writeOnly=" + writeOnly +
+                '}';
+    }
+}
